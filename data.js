31b7e13f9310d6cddd2862eb2703dab9e14cd1d0
Ahmad Ali <aallii300300@gmail.com>
2020-10-15 23:31:44 +0100

    Update data.js

--- data.js
+++ data.js
@@ -32,6 +32,81 @@ const cs_index = [
     definition:"The elements of the structure can change. ",
     reference: cite_uopeople_course(6),
   },
+  {
+  title: "list",
+    definition: "A sequence of values.",
+    reference: cite_think_python_book(100),
+  },
+    {
+  title: "list",
+    definition: "A sequence of values.",
+    reference: cite_think_python_book(100),
+  },
+    {
+  title: "element",
+    definition: "One of the values in a list (or other sequence), also called items.",
+    reference: cite_think_python_book(100),
+  },
+    {
+  title: "nested list",
+    definition: "A list that is an element of another list.",
+    reference: cite_think_python_book(100),
+  },
+    {
+  title: "accumlator",
+    definition: "A variable used in a loop to add up or accumulate a result.",
+    reference: cite_think_python_book(100),
+  },
+    {
+  title: "augmented assingment",
+    definition: "A statement that updates the value of a variable using an operator like +=.",
+    reference: cite_think_python_book(100),
+  },
+    {
+  title: "reduce",
+    definition: "A processing pattern that traverses a sequence and accumulates the elements into a single result.",
+    reference: cite_think_python_book(100),
+  },
+    {
+  title: "map",
+    definition: "A processing pattern that traverses a sequence and performs an operation on each element.",
+    reference: cite_think_python_book(100),
+  },
+    {
+  title: "filter",
+    definition: "A processing pattern that traverses a list and selects the elements that satisfy some criterion.",
+    reference: cite_think_python_book(100),
+  },
+    {
+  title: "object",
+    definition: "Something a variable can refer to. An object has a type and a value.",
+    reference: cite_think_python_book(100),
+  },
+    {
+  title: "equivalent",
+    definition: "Having the same value.",
+    reference: cite_think_python_book(100),
+  },
+    {
+  title: "identical",
+    definition: "Being the same object (which implies equivalence).",
+    reference: cite_think_python_book(100),
+  },
+    {
+  title: "reference",
+    definition: "The association between a variable and its value.",
+    reference: cite_think_python_book(100),
+  },
+    {
+  title: "aliasing",
+    definition: "A circumstance where two or more variables refer to the same object.",
+    reference: cite_think_python_book(100),
+  },
+    {
+  title: "delimiter",
+    definition: "A character or string used to indicate where a string should be split.",
+    reference: cite_think_python_book(100),
+  },
 ];
 
 const cs_index_titles_array = [];
